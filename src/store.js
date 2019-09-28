f1ed44369c08bd3e2c682cb4445b8e04052b555d
123 <ef_boudjedien@esi.dz>
2019-09-28 11:57:49 +0100

    store initialization

--- src/store.js
+++ src/store.js
@@ -4,7 +4,47 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    // state of loggedin
+    LoggedIn: localStorage.getItem("loggedin") || null,
+    // lists of todos
+    toDos: []
+  },
+  mutations: {
+    // login : mutate the state of loggedIn
+    login(state) {
+      state.LoggedIn = true;
+    },
+    // add to do item
+    addToDo(state, param) {
+      state.toDos.push(param);
+    },
+    // check toDoItem
+    checkOffToDo(state, param) {
+      state.toDos.find(element => {
+        return (element.id = param);
+      }).checked = true;
+    },
+    //remove toDoItem
+    removeToDo(state, param) {
+      state.toDos = state.toDos.filter(element => {
+        return element.id != param;
+      });
+    }
+  },
+  actions: {
+    LOGIN(context) {
+      context.commit("login");
+    },
+
+    ADD_TODO(context, todo) {
+      context.commit("addToDo", todo);
+    },
+    CHECK_OFF_TODO(context, todoId) {
+      context.commit("checkOffToDo", todoId);
+    },
+    REMOVE_TODO(context, todoId) {
+      context.commit("removeToDo");
+    }
+  }
 });
